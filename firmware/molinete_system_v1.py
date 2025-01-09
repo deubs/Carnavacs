572e9b715a54a766b09c91d4941218c1d5e2651c
Emiliano Melchiori <emelchiori@gmail.com>
2025-01-09 19:11:13 -0300

    update

--- firmware/molinete_system_v1.py
+++ firmware/molinete_system_v1.py
@@ -98,21 +98,21 @@ def readBarCodes(device, q: queue):
     print('begin reading...')
     barcode = ''
     try:
-        for event in device.read_loop():
-            if event.type == ecodes.EV_KEY:
-                eventdata = categorize(event)
-                if eventdata.keystate == 1: # Keydown
-                    scancode = eventdata.scancode
-                    if scancode == 28: # Enter
-                        if BCODEREAD_ENABLED:
-                            # saveBarcode(barcode)
-                            q.put(barcode)
-                            barcode = ''
-                    else:
-                        key = scancodes.get(scancode, NOT_RECOGNIZED_KEY)
-                        barcode = barcode + key
-                        if key == NOT_RECOGNIZED_KEY:
-                            print('unknown key, scancode=' + str(scancode))
+        if BCODEREAD_ENABLED:
+            for event in device.read_loop():
+                if event.type == ecodes.EV_KEY:
+                    eventdata = categorize(event)
+                    if eventdata.keystate == 1: # Keydown
+                        scancode = eventdata.scancode
+                        if scancode == 28: # Enter
+                                # saveBarcode(barcode)
+                                q.put(barcode)
+                                barcode = ''
+                        else:
+                            key = scancodes.get(scancode, NOT_RECOGNIZED_KEY)
+                            barcode = barcode + key
+                            if key == NOT_RECOGNIZED_KEY:
+                                print('unknown key, scancode=' + str(scancode))
     except Exception as e:
         print(e)
         idev = None
