7e1ad34cbdc04e9c3f7887f4b8e333562872c922
LPapps <patolencina51@gmail.com>
2025-01-25 19:17:45 -0300

    resolviendo url

--- dashboard/app/stores/API_URL.js
+++ dashboard/app/stores/API_URL.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-export const store_API_URL = create((set)=> ({ 
-    API_URL: false,
+export const store_API_URL = create((set)=> ({
+    API_URL: undefined,
     set_API_URL: (value) => set((st)=>({ API_URL: value }))
 }))
