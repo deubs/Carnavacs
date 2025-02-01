1cebdb29efea3228695ce3286eef8e24958d3c95
Emiliano Melchiori <emelchiori@gmail.com>
2025-02-01 16:14:11 -0300

    lcd

--- firmware/LCDI2Cv2.py
+++ firmware/LCDI2Cv2.py
@@ -72,22 +72,51 @@ print(platform.node())
 #         pass
 # I2C_ADDR = i2c
 
+
+from smbus2 import SMBus, i2c_msg
+
+# Define the addresses of the two LCD displays
+address1 = 0x27
+address2 = 0x3F
+
+# Define the data to be written to each display
+data1 = [40, 50]  # Example data for the first display
+data2 = [60, 70]  # Example data for the second display
+
+# Create i2c_msg objects for writing to each display
+write_msg1 = i2c_msg.write(address1, data1)
+write_msg2 = i2c_msg.write(address2, data2)
+
+# Perform the write operations
+with SMBus(1) as bus:
+    bus.i2c_rdwr(write_msg1, write_msg2)
+
+
+
 class LCD(object):
   
-    def lcd_init(self, i2caddress, bus):
-        print(hex(i2caddress))
-        self.i2caddress = i2caddress
-        self.bus = bus
+    def lcd_init(self, i2caddressa, i2caddressb):
+        # print(hex(i2caddress))
+        self.i2caddressa = i2caddressa
+        self.i2caddressb = i2caddressb
+        # self.bus = bus
     # Initialise display
-        self.lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-        self.lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-        self.lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-        self.lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
-        self.lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-        self.lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+        self.lcd_byte(0x33,LCD_CMD, self.i2caddressa) # 110011 Initialise
+        self.lcd_byte(0x32,LCD_CMD, self.i2caddressa) # 110010 Initialise
+        self.lcd_byte(0x06,LCD_CMD, self.i2caddressa) # 000110 Cursor move direction
+        self.lcd_byte(0x0C,LCD_CMD, self.i2caddressa) # 001100 Display On,Cursor Off, Blink Off 
+        self.lcd_byte(0x28,LCD_CMD, self.i2caddressa) # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01,LCD_CMD, self.i2caddressa) # 000001 Clear display
+        self.lcd_byte(0x33,LCD_CMD, self.i2caddressb) # 110011 Initialise
+        self.lcd_byte(0x32,LCD_CMD, self.i2caddressb) # 110010 Initialise
+        self.lcd_byte(0x06,LCD_CMD, self.i2caddressb) # 000110 Cursor move direction
+        self.lcd_byte(0x0C,LCD_CMD, self.i2caddressb) # 001100 Display On,Cursor Off, Blink Off 
+        self.lcd_byte(0x28,LCD_CMD, self.i2caddressb) # 101000 Data length, number of lines, font size
+        self.lcd_byte(0x01,LCD_CMD, self.i2caddressb) # 000001 Clear display
+
         time.sleep(E_DELAY)
 
-    def lcd_byte(self, bits, mode):
+    def lcd_byte(self, bits, mode, addr):
         # Send byte to data pins
         # bits = the data
         # mode = 1 for data
@@ -95,27 +124,27 @@ class LCD(object):
         bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
         bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
         # High bits
-        self.bus.write_byte(self.i2caddress, bits_high)
-        self.lcd_toggle_enable(bits_high)
+        bus.write_byte(addr, bits_high)
+        self.lcd_toggle_enable(bits_high, addr)
         # Low bits
-        self.bus.write_byte(self.i2caddress, bits_low)
-        self.lcd_toggle_enable(bits_low)
+        bus.write_byte(addr, bits_low)
+        self.lcd_toggle_enable(bits_low, addr)
 
 
-    def lcd_toggle_enable(self, bits):
+    def lcd_toggle_enable(self, bits, addr):
     # Toggle enable
         time.sleep(E_DELAY)
-        self.bus.write_byte(self.i2caddress, (bits | ENABLE))
+        bus.write_byte(addr, (bits | ENABLE))
         time.sleep(E_PULSE)
-        self.bus.write_byte(self.i2caddress, (bits & ~ENABLE))
+        bus.write_byte(addr, (bits & ~ENABLE))
         time.sleep(E_DELAY)
 
 
-    def lcd_string(self, message, line):
+    def lcd_string(self, message, line, addr):
     # Send string to display
         message = message.ljust(LCD_WIDTH," ")
         
-        self.lcd_byte(line, LCD_CMD)
+        self.lcd_byte(line, LCD_CMD, addr)
         for i in range(LCD_WIDTH):
             self.lcd_byte(ord(message[i]),LCD_CHR)
 
