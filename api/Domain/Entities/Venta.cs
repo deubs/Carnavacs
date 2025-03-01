3dcaf9bed50640838c5aadaa32b7587c1a1a1fb7
Hernando Rojas <hernando.rojas@gmail.com>
2025-03-01 16:13:24 -0400

    Venta Entity

---
+++ api/Domain/Entities/Venta.cs
@@ -0,0 +1,10 @@
+﻿namespace Carnavacs.Api.Domain.Entities
+{
+    public class Venta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public  bool Habilitado { get; set; }
+
+    }
+}
