8a32fca680212364ad46c7501eb58dda6361f360
Emiliano Melchiori <emelchiori@gmail.com>
2026-01-02 20:03:05 -0300

    qr

--- firmware/molinete_system_v1.py
+++ firmware/molinete_system_v1.py
@@ -99,14 +99,14 @@ scancodes = {
     10: u'9', 11: u'0', 12: u'-', 13: u'=', 14: u'BKSP', 15: u'TAB', 16: u'Q', 17: u'W', 18: u'E', 19: u'R',
     20: u'T', 21: u'Y', 22: u'U', 23: u'I', 24: u'O', 25: u'P', 26: u'[', 27: u']', 28: u'CRLF', 29: u'LCTRL',
     30: u'A', 31: u'S', 32: u'D', 33: u'F', 34: u'G', 35: u'H', 36: u'J', 37: u'K', 38: u'L', 39: u';',
-    40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
+    40: u'"', 41: u'`', 43: u'\\', 44: u'Z', 45: u'X', 46: u'C', 47: u'V', 48: u'B', 49: u'N',
     50: u'M', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 100: u'RALT'
 }
 
 idev = None
 sp = None
 
-NOT_RECOGNIZED_KEY = u'X'
+NOT_RECOGNIZED_KEY = u''
 l1 = LCDI2C.LCD_LINE_1
 l2 = LCDI2C.LCD_LINE_2
 
